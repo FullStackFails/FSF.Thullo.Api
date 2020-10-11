2a887c783f8911885e664b6af3c5dfbe78c05a62
Kyle Hoff <kylehoff01@gmail.com>
2020-10-10 21:00:16 -0600

    Added BoardsController with Get endpoint. Modified service configuration and set default route for IISExpress.

---
+++ FSF.Thullo.Api/Controllers/BoardsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FSF.Thullo.Api.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BoardsController : ControllerBase
+  {
+    [HttpGet]
+    public IActionResult Get()
+    {
+      return Ok(new string[] { "Board_1", "Board_2" });
+    }
+  }
+}
