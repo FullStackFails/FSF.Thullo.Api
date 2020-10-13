9cff3d81441485f5497dac3dc33d7d6f3b8940e2
Kyle Hoff <kylehoff01@gmail.com>
2020-10-12 21:01:58 -0600

    Missed changes in commit

---
+++ FSF.Thullo.Data/tables/List.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[List]
+(
+  [Id] INT NOT NULL PRIMARY KEY IDENTITY,
+  [Title] VARCHAR(100) NOT NULL,
+  [BoardId] INT NOT NULL,
+  [CreatedDate] DATETIME2 NOT NULL DEFAULT GetDate(),
+  [CreatedBy] INT NULL, -- To correlate a userId in the future
+  [ModifiedDate] DATETIME2 NOT NULL DEFAULT GetDate(),
+
+  CONSTRAINT FK_Board FOREIGN KEY (BoardId)
+  REFERENCES dbo.Board (Id)
+)
