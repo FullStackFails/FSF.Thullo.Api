a33bb499b61563fe691e18403c7988cf81272459
pclover <pcloverdevelopment@outlook.com>
2020-10-19 18:32:14 -0600

    set up a simple example of health check test

---
+++ FSF.Thullo.Api.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace FSF.Thullo.Api.IntegrationTests
+{
+  /// <summary>
+  /// This class is used to help with setup of Microsoft's
+  /// Mvc Testing class 'WebApplicationFactory'..
+  /// </summary>
+  /// <see cref="https://github.com/willj/aspnet-core-mstest-integration-sample"/>
+  public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<Startup>
+  {
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+      base.ConfigureWebHost(builder);
+    }
+  }
+}
---
+++ FSF.Thullo.Api.IntegrationTests/Middleware/HealthCheckTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FSF.Thullo.Api.IntegrationTests.Middleware
+{
+  [TestClass]
+  public class HealthCheckTests
+  {
+    private readonly HttpClient _client;
+    private readonly CustomWebApplicationFactory<Startup> _factory;
+
+    public HealthCheckTests()
+    {
+      _factory = new CustomWebApplicationFactory<Startup>();
+      _client = _factory.CreateDefaultClient();
+    }
+
+    [TestMethod]
+    public async Task HealthCheck_ReturnsOk()
+    {
+      var response = await _client.GetAsync("/healthcheck");
+
+      response.EnsureSuccessStatusCode();
+    }
+  }
+}
