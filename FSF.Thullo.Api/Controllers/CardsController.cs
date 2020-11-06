93ec3b59b7bf90c728923ff01be1f615ca585f65
Kyle Hoff <kylehoff01@gmail.com>
2020-11-05 21:53:28 -0700

    closes #20. Created Cards Api and setup db connections in the thullo service instead of the repository. May want to seperate the service by resources.

---
+++ FSF.Thullo.Api/Controllers/CardsController.cs
@@ -0,0 +1,53 @@
+﻿using FSF.Thullo.Core.Entities;
+using FSF.Thullo.Core.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace FSF.Thullo.Api.Controllers
+{
+  [Route("api/boards/{boardId}/lists/{listId}/cards")]
+  [ApiController]
+  public class CardsController : ControllerBase
+  {
+    private readonly ThulloService _thulloService;
+
+    public CardsController(ThulloService thulloService)
+    {
+      _thulloService = thulloService;
+    }
+
+    [HttpGet]
+    public IActionResult Get(int boardId, int listId)
+    {
+      return Ok(_thulloService.GetCards(boardId, listId));
+    }
+
+    [HttpGet]
+    [Route("{cardId}")]
+    public IActionResult Get(int boardId, int listId, int cardId)
+    {
+      return Ok(_thulloService.GetCard(boardId, listId, cardId));
+    }
+
+    [HttpPost]
+    public IActionResult Post(int boardId, int listId, Card card)
+    {
+      return Created(string.Empty, _thulloService.CreateCard(boardId, listId, card));
+    }
+
+    [HttpPut]
+    [Route("{cardId}")]
+    public IActionResult Put(int boardId, int listId, int cardId, Card card)
+    {
+      return Ok(_thulloService.UpdateCard(boardId, listId, cardId, card));
+    }
+
+    [HttpDelete]
+    [Route("{cardId}")]
+    public IActionResult Delete(int boardId, int listId, int cardId)
+    {
+      _thulloService.DeleteCard(boardId, listId, cardId);
+      return Ok();
+    }
+  }
+}
--- FSF.Thullo.Core/Interfaces/DataAccess/IThulloRepository.cs
+++ FSF.Thullo.Core/Interfaces/DataAccess/IThulloRepository.cs
@@ -1,23 +1,28 @@
 ﻿using FSF.Thullo.Core.Entities;
-using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Data;
 
 namespace FSF.Thullo.Core.Interfaces.DataAccess
 {
   public interface IThulloRepository
   {
     // Boards
-    Board CreateBoard(Board board);
-    IEnumerable<Board> GetBoards();
-    Board GetBoard(int boardId);
-    Board UpdateBoard(int boardId, Board board);
-    void DeleteBoard(int boardId);
+    Board CreateBoard(IDbConnection db, Board board);
+    IEnumerable<Board> GetBoards(IDbConnection db);
+    Board GetBoard(IDbConnection db, int boardId);
+    Board UpdateBoard(IDbConnection db, int boardId, Board board);
+    void DeleteBoard(IDbConnection db, int boardId);
     // Lists
-    List CreateList(int boardId, List list);
-    IEnumerable<List> GetLists(int boardId);
-    List GetList(int listId);
-    List UpdateList(int listId, List list);
-    void DeleteList(int listId);
+    List CreateList(IDbConnection db, int boardId, List list);
+    IEnumerable<List> GetLists(IDbConnection db, int boardId);
+    List GetList(IDbConnection db, int boardId, int listId);
+    List UpdateList(IDbConnection db, int boardId, int listId, List list);
+    void DeleteList(IDbConnection db, int boardId, int listId);
+    // Cards
+    Card CreateCard(IDbConnection db, int boardId, int listId, Card card);
+    IEnumerable<Card> GetCards(IDbConnection db, int boardId, int listId);
+    Card GetCard(IDbConnection db, int boardId, int listId, int cardId);
+    Card UpdateCard(IDbConnection db, int boardId, int listId, int cardId, Card card);
+    void DeleteCard(IDbConnection db, int boardId, int listId, int cardId);
   }
 }
