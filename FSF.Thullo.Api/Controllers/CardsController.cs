2a9b22257750b8903adf2325c8754d474c21b043
Patrick Clover <pcloverdevelopment@outlook.com>
2020-12-13 10:35:28 -0700

    Added xml commenting to the cards controller.

--- FSF.Thullo.Api/Controllers/CardsController.cs
+++ FSF.Thullo.Api/Controllers/CardsController.cs
@@ -1,23 +1,36 @@
 ﻿using FSF.Thullo.Core.Dto.CardDtos;
-using FSF.Thullo.Core.Entities;
 using FSF.Thullo.Core.Services;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Linq;
 
 namespace FSF.Thullo.Api.Controllers
 {
+  /// <summary>
+  /// This controller provides an interface for working with cards.
+  /// Cards have a many to one relationship with a given list.
+  /// A Card is a subresources of a List.
+  /// </summary>
   [Route("api/boards/{boardId}/lists/{listId}/cards")]
   [ApiController]
   public class CardsController : ControllerBase
   {
     private readonly ThulloService _thulloService;
 
+    /// <summary>
+    /// Constructor for the cards controller.
+    /// </summary>
+    /// <param name="thulloService">Delegation service for Thullo business logic.</param>
     public CardsController(ThulloService thulloService)
     {
       _thulloService = thulloService;
     }
 
+    /// <summary>
+    /// Gets all cards belonging to a list.
+    /// </summary>
+    /// <param name="boardId">The id of the board.</param>
+    /// <param name="listId">The id of the list.</param>
+    /// <returns>A collection of cards.</returns>
     [HttpGet]
     public IActionResult Get(int boardId, int listId)
     {
@@ -26,6 +39,13 @@ namespace FSF.Thullo.Api.Controllers
       return Ok(cards);
     }
 
+    /// <summary>
+    /// Gets a single card.
+    /// </summary>
+    /// <param name="boardId">The id of the board.</param>
+    /// <param name="listId">The id of the list.</param>
+    /// <param name="cardId">The id of the card.</param>
+    /// <returns>A single card.</returns>
     [HttpGet]
     [Route("{cardId}")]
     public IActionResult Get(int boardId, int listId, int cardId)
@@ -34,6 +54,13 @@ namespace FSF.Thullo.Api.Controllers
       return Ok(card);
     }
 
+    /// <summary>
+    /// Creates a new card associated to a list.
+    /// </summary>
+    /// <param name="boardId">The id of the board.</param>
+    /// <param name="listId">The id of the list.</param>
+    /// <param name="dto">The representation of the new card.</param>
+    /// <returns>The newly created card.</returns>
     [HttpPost]
     public IActionResult Post(int boardId, int listId, CardforCreationDto dto)
     {
@@ -45,6 +72,14 @@ namespace FSF.Thullo.Api.Controllers
       return Created(string.Empty, createdCard);
     }
 
+    /// <summary>
+    /// Updates a card.
+    /// </summary>
+    /// <param name="boardId">The id of the board.</param>
+    /// <param name="listId">The id of the list.</param>
+    /// <param name="cardId">The id of the card.</param>
+    /// <param name="dto">The updated representation of the card.</param>
+    /// <returns>The updated card.</returns>
     [HttpPut]
     [Route("{cardId}")]
     public IActionResult Put(int boardId, int listId, int cardId, CardForUpdateDto dto)
@@ -58,6 +93,12 @@ namespace FSF.Thullo.Api.Controllers
       return Ok(updatedCard);
     }
 
+    /// <summary>
+    /// Deletes a card.
+    /// </summary>
+    /// <param name="boardId">The id of the board.</param>
+    /// <param name="listId">The id of the list.</param>
+    /// <param name="cardId">The id of the card.</param>
     [HttpDelete]
     [Route("{cardId}")]
     public IActionResult Delete(int boardId, int listId, int cardId)
